a72d704108ff697e4af39e9121d06da76d0ebe5d
ck kim <keegi@kakao.com>
2022-08-25 19:49:36 +0900

    movie app

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Web movie 2019
 
 React JS Fundamentals Course (2019 Update!)
+
+npm install prop-types./
+npm install axios./
+npm install gh-pages
--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
 class App extends Component {
+
+  state = {
+    isLoading: true,
+    movie: []
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: {movies}
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({ movies, isLoading: false });
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+    this.getMovies();
+  };
+
   render() {
+
+    const { isLoading, movies } = this.state;
+
     return (
-      <div>
-        <h1>Hello!!</h1>
-      </div>
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+                <Movie 
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              )
+            )}
+          </div>
+        )}
+      </section>
     )
   }
 }
